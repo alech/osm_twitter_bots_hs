9bb064d6d6be7e0d06be7e6f3fd064ff465d8704
Alexander Klink <git@alech.de>
2012-07-20 22:10:38 +0200

    extract comment, too

--- OSM.hs
+++ OSM.hs
@@ -27,6 +27,7 @@ data OSMChangeSet = OSMChangeSet {
 qN :: String -> QName
 qN qn = QName qn Nothing Nothing
 
+-- use only on attributes you know to exist (otherwise fromJust fails)
 attr :: String -> Element -> String
 attr key =
 	fromJust . findAttr (QName key Nothing Nothing)
@@ -34,10 +35,20 @@ attr key =
 doubleAttr :: String -> Element -> Double
 doubleAttr key elem = read $ attr key elem :: Double
 
+-- extract the comment value from a changeset element, given the following format
+-- <changeset ...>
+--     <tag k="comment" v="Modified via wheelmap.org"/>
+-- </changeset>
+extractComment :: Element -> String
+extractComment changesetElem =
+	concat $ mapMaybe extractValue $ filterElements isCommentKey changesetElem
+	where isCommentKey e = (findAttr (qN "k") e) == Just "comment"
+	      extractValue = findAttr (qN "v")
+
 -- get a list of changesets from an OSM API reply
 changeSetsFromXML :: [Content] -> [OSMChangeSet]
 changeSetsFromXML xml =
-	concat $ map osmElementToChangeSets $ osmElements xml
+	concatMap osmElementToChangeSets $ osmElements xml
 	where osmElements xml = mapMaybe findOsmElem xmlElements
 		where xmlElements = onlyElems xml
 		      findOsmElem = findElement (qN "osm")
@@ -53,7 +64,7 @@ changesetElementToChangeSet changesetElem =
 	where csId = read $ attr "id" changesetElem :: Integer
 	      user = attr "user" changesetElem
 	      open = "true" == attr "open" changesetElem
-	      comment = "" -- FIXME
+	      comment = extractComment changesetElem
 	      bb   = BoundingBox minLat minLon maxLat maxLon
 	      	where minLat = doubleAttr "min_lat" changesetElem
 	      	      minLon = doubleAttr "min_lon" changesetElem
